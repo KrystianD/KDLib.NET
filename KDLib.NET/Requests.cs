d4b519188d555979fb24f6c3e1d930179f7724c6
Krystian Dużyński <krystian.duzynski@gmail.com>
2019-03-03 04:31:01 +0100

    Requests: fixed HttpClient usage, added tests

--- KDLib.NET/Requests.cs
+++ KDLib.NET/Requests.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using Newtonsoft.Json;
@@ -18,9 +19,13 @@ namespace KDLib
 {
   public static class Requests
   {
+    private static readonly HttpClient httpClient = new HttpClient();
+
     public class RequestOptions
     {
       public bool LoggingEnabled = true;
+      public bool LogPayload = true;
+      public bool AlwaysLogFailures = true;
     }
 
     public class Response
@@ -85,9 +90,10 @@ namespace KDLib
         string authPass = null,
         string authBearerToken = null,
         TimeSpan? timeout = null,
-        RequestOptions options = null)
+        RequestOptions options = null,
+        CancellationToken cancellationToken = default)
     {
-      return DoRequest(HttpMethod.Get, url, parameters, null, null, headers, authUser, authPass, authBearerToken, timeout, options: options);
+      return DoRequest(HttpMethod.Get, url, parameters, null, null, headers, authUser, authPass, authBearerToken, timeout, options: options, cancellationToken: cancellationToken);
     }
 
     public static Task<Response> Post(
@@ -100,9 +106,10 @@ namespace KDLib
         string authPass = null,
         string authBearerToken = null,
         TimeSpan? timeout = null,
-        RequestOptions options = null)
+        RequestOptions options = null,
+        CancellationToken cancellationToken = default)
     {
-      return DoRequest(HttpMethod.Post, url, parameters, data, json, headers, authUser, authPass, authBearerToken, timeout, options: options);
+      return DoRequest(HttpMethod.Post, url, parameters, data, json, headers, authUser, authPass, authBearerToken, timeout, options: options, cancellationToken: cancellationToken);
     }
 
     public static Task<Response> Put(
@@ -115,9 +122,10 @@ namespace KDLib
         string authPass = null,
         string authBearerToken = null,
         TimeSpan? timeout = null,
-        RequestOptions options = null)
+        RequestOptions options = null,
+        CancellationToken cancellationToken = default)
     {
-      return DoRequest(HttpMethod.Put, url, parameters, data, json, headers, authUser, authPass, authBearerToken, timeout, options: options);
+      return DoRequest(HttpMethod.Put, url, parameters, data, json, headers, authUser, authPass, authBearerToken, timeout, options: options, cancellationToken: cancellationToken);
     }
 
     private static async Task<Response> DoRequest(
@@ -131,12 +139,12 @@ namespace KDLib
         string authPass = null,
         string authBearerToken = null,
         TimeSpan? timeout = null,
-        RequestOptions options = null)
+        RequestOptions options = null,
+        CancellationToken cancellationToken = default)
     {
       if (options == null)
         options = new RequestOptions();
 
-      var client = new HttpClient();
 
       url = UrlUtils.CreateUrl(url, parameters);
 
@@ -166,60 +174,95 @@ namespace KDLib
       if (!timeout.HasValue)
         timeout = TimeSpan.FromSeconds(60);
 
-      client.Timeout = timeout.Value.Add(TimeSpan.FromSeconds(10));
-
       string requestId = StringUtils.GenerateRandomString(4);
-      if (options.LoggingEnabled)
-        LogManager.GetLogger("requests")
-                  .Trace()
-                  .Message($"Sending request ({requestId}) to {method} {url}")
-                  .Property("request_id", requestId)
-                  .Property("parameters", parameters == null ? null : JsonConvert.SerializeObject(parameters))
-                  .Property("data", data == null ? null : JsonConvert.SerializeObject(data))
-                  .Property("json", json)
-                  .Property("headers", headers == null ? null : JsonConvert.SerializeObject(headers))
-                  .Write();
+      if (options.LoggingEnabled) {
+        var b = LogManager.GetLogger("requests")
+                          .Trace()
+                          .Message($"Sending request ({requestId}) to {method} {url}")
+                          .Property("request_id", requestId)
+                          .Property("parameters", parameters == null ? null : JsonConvert.SerializeObject(parameters))
+                          .Property("headers", headers == null ? null : JsonConvert.SerializeObject(headers));
 
+        if (options.LogPayload) {
+          b.Property("data", data == null ? null : JsonConvert.SerializeObject(data));
+          b.Property("json", json);
+        }
+
+        b.Write();
+      }
+
+      var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+      if (timeout != Timeout.InfiniteTimeSpan)
+        cts.CancelAfter(timeout.Value);
+      
       var fetchTask = Task.Run(async () =>
       {
-        var response = await client.SendAsync(request).ConfigureAwait(false);
+        var response = await httpClient.SendAsync(request, cts.Token).ConfigureAwait(false);
         var respData = await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
         return new Response(response, respData);
-      });
+      }, cts.Token);
 
+      var stopwatch = Stopwatch.StartNew();
       try {
-        var sw = Stopwatch.StartNew();
-        var resp = await AsyncUtils.WaitFutureTimeout(fetchTask, timeout.Value);
+        var resp = await AsyncUtils.FastCancellableTask(fetchTask, cts.Token);
 
         int codeGroup = (int) resp.StatusCode / 100;
         if (codeGroup.In(2, 3)) {
-          if (options.LoggingEnabled)
-            LogManager.GetLogger("requests")
-                      .Trace()
-                      .Message($"Received response ({requestId}) from {method} {url} ({sw.ElapsedMilliseconds}ms)")
-                      .Property("response", resp.FormatResponse())
-                      .Property("request_id", requestId)
-                      .Write();
+          if (options.LoggingEnabled) {
+            var b = LogManager.GetLogger("requests")
+                              .Trace()
+                              .Message($"Received response ({requestId}) from {method} {url} ({stopwatch.ElapsedMilliseconds}ms)")
+                              .Property("request_id", requestId);
+
+            if (options.LogPayload) {
+              b.Property("response", resp.FormatResponse());
+            }
+
+            b.Write();
+          }
         }
         else {
-          LogManager.GetLogger("requests")
-                    .Warn()
-                    .Message($"Request ({requestId}) to {method} {url} failed with code: {(int) resp.StatusCode} ({sw.ElapsedMilliseconds}ms)")
-                    .Property("status_code", (int) resp.StatusCode)
-                    .Property("response", resp.FormatResponse())
-                    .Property("request_id", requestId)
-                    .Write();
+          if (options.LoggingEnabled || options.AlwaysLogFailures) {
+            var b = LogManager.GetLogger("requests")
+                              .Warn()
+                              .Message($"Request ({requestId}) to {method} {url} failed with code: {(int) resp.StatusCode} ({stopwatch.ElapsedMilliseconds}ms)")
+                              .Property("response", resp.FormatResponse())
+                              .Property("request_id", requestId);
+
+            if (options.LogPayload) {
+              b.Property("status_code", (int) resp.StatusCode);
+            }
+
+            b.Write();
+          }
         }
 
         return resp;
       }
+      catch (OperationCanceledException) when (!cancellationToken.IsCancellationRequested) {
+        if (options.LoggingEnabled || options.AlwaysLogFailures) {
+          LogManager.GetLogger("requests")
+                    .Warn()
+                    .Message($"Request ({requestId}) to {method} {url} timed out: ({stopwatch.ElapsedMilliseconds}ms)")
+                    .Property("request_id", requestId)
+                    .Write();
+        }
+
+        throw new TimeoutException();
+      }
+      catch (OperationCanceledException) when (cancellationToken.IsCancellationRequested) {
+        throw new TaskCanceledException();
+      }
       catch (Exception e) {
-        LogManager.GetLogger("requests")
-                  .Warn()
-                  .Message($"Request ({requestId}) to {method} {url} failed: {e.Message}")
-                  .Property("request_id", requestId)
-                  .Exception(e)
-                  .Write();
+        if (options.LoggingEnabled || options.AlwaysLogFailures) {
+          LogManager.GetLogger("requests")
+                    .Warn()
+                    .Message($"Request ({requestId}) to {method} {url} failed: {e.Message} ({stopwatch.ElapsedMilliseconds}ms)")
+                    .Property("request_id", requestId)
+                    .Exception(e)
+                    .Write();
+        }
+
         throw;
       }
     }
