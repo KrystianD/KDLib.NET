148c67ad0fa74051e3c1417b38b983a0aaee5f9c
Krystian Dużyński <krystian.duzynski@gmail.com>
2020-07-14 22:12:53 +0200

    added AdvancedJsonDateTimeConverter

---
+++ KDLib.NET.Tests/JsonConvertersTests.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using KDLib.JsonConverters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace KDLib.NET.Tests
+{
+  public class JsonConvertersTests
+  {
+    public class Model
+    {
+      [JsonConverter(typeof(AdvancedJsonDateTimeConverter), AdvancedJsonDateTimeConverter.Mode.SeparatorSpace)]
+      public DateTime date_space;
+
+      [JsonConverter(typeof(AdvancedJsonDateTimeConverter), AdvancedJsonDateTimeConverter.Mode.SeparatorT)]
+      public DateTime date_t;
+
+      [JsonConverter(typeof(AdvancedJsonDateTimeConverter), AdvancedJsonDateTimeConverter.Mode.SeparatorT |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithSeconds)]
+      public DateTime date_t_seconds;
+
+      [JsonConverter(typeof(AdvancedJsonDateTimeConverter), AdvancedJsonDateTimeConverter.Mode.SeparatorT |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithSeconds |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithZ)]
+      public DateTime date_t_seconds_z;
+
+      [JsonConverter(typeof(AdvancedJsonDateTimeConverter), AdvancedJsonDateTimeConverter.Mode.SeparatorT |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithSeconds |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithMilliseconds3 |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithZ)]
+      public DateTime date_t_seconds_ms3_z;
+
+      [JsonConverter(typeof(AdvancedJsonDateTimeConverter), AdvancedJsonDateTimeConverter.Mode.SeparatorT |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithSeconds |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithMilliseconds6 |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithZ)]
+      public DateTime date_t_seconds_ms6_z;
+
+      [JsonConverter(typeof(AdvancedJsonDateTimeConverter), AdvancedJsonDateTimeConverter.Mode.SeparatorT |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithSeconds |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithOffset)]
+      public DateTime date_t_seconds_tz1;
+
+      [JsonConverter(typeof(AdvancedJsonDateTimeConverter), AdvancedJsonDateTimeConverter.Mode.SeparatorT |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithSeconds |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithMilliseconds3 |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithOffset)]
+      public DateTime date_t_seconds_ms3_tz1;
+
+      [JsonConverter(typeof(AdvancedJsonDateTimeConverter), AdvancedJsonDateTimeConverter.Mode.SeparatorT |
+                                                            AdvancedJsonDateTimeConverter.Mode.AsUTC)]
+      public DateTime date_t_utc;
+
+      [JsonConverter(typeof(AdvancedJsonDateTimeConverter), AdvancedJsonDateTimeConverter.Mode.SeparatorT |
+                                                            AdvancedJsonDateTimeConverter.Mode.WithZ |
+                                                            AdvancedJsonDateTimeConverter.Mode.AsUTC)]
+      public DateTime date_t_utc_z;
+    }
+
+    private static Model ParseJson(object data) =>
+        JsonConvert.DeserializeObject<Model>(JToken.FromObject(data).ToString(), new JsonSerializerSettings { DateParseHandling = DateParseHandling.None });
+
+    private static DateTime CreateDateTime(int year, int month, int days, int hours, int minutes, int seconds, int microseconds) =>
+        DateTime.SpecifyKind(new DateTime(year, month, days, hours, minutes, seconds).AddTicks(microseconds * 10), DateTimeKind.Unspecified);
+
+    [Fact]
+    public void DateSpace()
+    {
+      Assert.Equal(CreateDateTime(2345, 10, 20, 12, 34, 00, 0), ParseJson(new {
+          date_space = "2345-10-20 12:34",
+      }).date_space);
+    }
+
+    [Fact]
+    public void DateT()
+    {
+      Assert.Equal(CreateDateTime(2345, 10, 20, 12, 34, 00, 0), ParseJson(new {
+          date_t = "2345-10-20T12:34",
+      }).date_t);
+    }
+
+    [Fact]
+    public void DateTSeconds()
+    {
+      Assert.Equal(CreateDateTime(2345, 10, 20, 12, 34, 56, 0), ParseJson(new {
+          date_t_seconds = "2345-10-20T12:34:56",
+      }).date_t_seconds);
+    }
+
+    [Fact]
+    public void DateTSecondsZ()
+    {
+      Assert.Equal(CreateDateTime(2345, 10, 20, 12, 34, 56, 0), ParseJson(new {
+          date_t_seconds_z = "2345-10-20T12:34:56Z",
+      }).date_t_seconds_z);
+    }
+
+    [Fact]
+    public void DateTSecondsMillisecondsZ()
+    {
+      Assert.Equal(CreateDateTime(2345, 10, 20, 12, 34, 56, 123000), ParseJson(new {
+          date_t_seconds_ms3_z = "2345-10-20T12:34:56.123Z",
+      }).date_t_seconds_ms3_z);
+
+      Assert.Equal(CreateDateTime(2345, 10, 20, 12, 34, 56, 123456), ParseJson(new {
+          date_t_seconds_ms6_z = "2345-10-20T12:34:56.123456Z",
+      }).date_t_seconds_ms6_z);
+    }
+
+    [Fact]
+    public void DateTSecondsTz()
+    {
+      Assert.Equal(CreateDateTime(2345, 10, 20, 12 + 2, 34, 56, 0), ParseJson(new {
+          date_t_seconds_tz1 = "2345-10-20T12:34:56-02:00",
+      }).date_t_seconds_tz1);
+    }
+
+    [Fact]
+    public void DateTSecondsMs3Tz()
+    {
+      Assert.Equal(CreateDateTime(2345, 10, 20, 12 + 2, 34, 56, 123000), ParseJson(new {
+          date_t_seconds_ms3_tz1 = "2345-10-20T12:34:56.123-02:00",
+      }).date_t_seconds_ms3_tz1);
+    }
+
+    [Fact]
+    public void DateTUTC()
+    {
+      Assert.Equal(CreateDateTime(2345, 10, 20, 12, 34, 00, 0), ParseJson(new {
+          date_t_utc = "2345-10-20T12:34",
+      }).date_t_utc);
+
+      Assert.Equal(CreateDateTime(2345, 10, 20, 12, 34, 00, 0), ParseJson(new {
+          date_t_utc_z = "2345-10-20T12:34Z",
+      }).date_t_utc_z);
+    }
+  }
+}
--- KDLib.NET/JsonConverters.cs
+++ KDLib.NET/JsonConverters.cs
@@ -13,6 +13,12 @@ namespace KDLib
       {
         DateTimeFormat = format;
       }
+
+      public DateFormatJsonConverter(string format, DateTimeStyles styles)
+      {
+        DateTimeFormat = format;
+        DateTimeStyles = styles;
+      }
     }
 
     public class UnixEpochMillisecondsJsonConverter : DateTimeConverterBase
@@ -39,7 +45,7 @@ namespace KDLib
         bool flag = IsNullableType(objectType);
         if (reader.TokenType == JsonToken.Null) {
           if (!flag)
-            throw new JsonSerializationException(string.Format("Cannot convert null value to {0}.", CultureInfo.InvariantCulture, objectType));
+            throw new JsonSerializationException(string.Format("Cannot convert null value to {0}.", objectType));
           return null;
         }
 
@@ -47,18 +53,18 @@ namespace KDLib
         if (reader.TokenType == JsonToken.Date) {
           if (type == typeof(DateTimeOffset)) {
             if (!(reader.Value is DateTimeOffset))
-              return new DateTimeOffset((DateTime) reader.Value);
+              return new DateTimeOffset((DateTime)reader.Value);
             return reader.Value;
           }
 
           object obj;
           if ((obj = reader.Value) is DateTimeOffset)
-            return ((DateTimeOffset) obj).DateTime;
+            return ((DateTimeOffset)obj).DateTime;
           return reader.Value;
         }
 
         if (reader.TokenType != JsonToken.Integer && reader.TokenType != JsonToken.String)
-          throw new JsonSerializationException(string.Format("Unexpected token parsing date. Expected Integer or String, got {0}.", CultureInfo.InvariantCulture, reader.TokenType));
+          throw new JsonSerializationException(string.Format("Unexpected token parsing date. Expected Integer or String, got {0}.", reader.TokenType));
         string str = reader.Value.ToString();
         if (string.IsNullOrEmpty(str) & flag)
           return null;
@@ -87,8 +93,115 @@ namespace KDLib
 
       public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
       {
-        writer.WriteValue(((decimal) value).ToString(CultureInfo.InvariantCulture));
+        writer.WriteValue(((decimal)value).ToString(CultureInfo.InvariantCulture));
       }
+    }
+
+    public class AdvancedJsonDateTimeConverter : DateTimeConverterBase
+    {
+      private readonly Mode _mode;
+
+      [Flags]
+      public enum Mode
+      {
+        SeparatorT = 1,
+        SeparatorSpace = 2,
+        WithSeconds = 4,
+        WithMilliseconds3 = 8,
+        WithMilliseconds6 = 16,
+        WithZ = 32,
+        WithOffset = 64,
+        AsUnspecified = 128,
+        AsUTC = 256,
+      }
+
+      public override bool CanRead => true;
+      public override bool CanWrite => false;
+
+      public override bool CanConvert(Type objectType)
+      {
+        return objectType == typeof(DateTime) || objectType == typeof(DateTime?);
+      }
+
+      public AdvancedJsonDateTimeConverter(Mode mode)
+      {
+        if (mode.HasFlag(Mode.SeparatorT) && mode.HasFlag(Mode.SeparatorSpace))
+          throw new Exception("invalid mode");
+        if (mode.HasFlag(Mode.WithMilliseconds3) && mode.HasFlag(Mode.WithMilliseconds6))
+          throw new Exception("invalid mode");
+        if ((mode.HasFlag(Mode.WithMilliseconds3) || mode.HasFlag(Mode.WithMilliseconds6)) && !mode.HasFlag(Mode.WithSeconds))
+          throw new Exception("invalid mode");
+        if (mode.HasFlag(Mode.WithZ) && mode.HasFlag(Mode.WithOffset))
+          throw new Exception("invalid mode");
+        if (mode.HasFlag(Mode.AsUnspecified) && mode.HasFlag(Mode.AsUTC))
+          throw new Exception("invalid mode");
+
+        _mode = mode;
+      }
+
+      public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+      {
+        throw new NotImplementedException();
+      }
+
+      public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+      {
+        bool flag = IsNullableType(objectType);
+        if (reader.TokenType == JsonToken.Null) {
+          if (!flag)
+            throw new JsonSerializationException($"Cannot convert null value to {objectType}.");
+          return null;
+        }
+
+        if (reader.TokenType == JsonToken.Date)
+          throw new Exception("Disable dates parsing with \"new JsonSerializerSettings { DateParseHandling = DateParseHandling.None }\"");
+
+        if (reader.TokenType != JsonToken.String)
+          throw new JsonSerializationException($"Unexpected token parsing date. Expected String, got {reader.TokenType}.");
+
+        string str = (string)reader.Value;
+        if (string.IsNullOrEmpty(str) & flag)
+          return null;
+
+        return Convert(str);
+      }
+
+      private DateTime Convert(string input)
+      {
+        string format = "yyyy-MM-dd";
+
+        if (_mode.HasFlag(Mode.SeparatorT))
+          format += "T";
+        else if (_mode.HasFlag(Mode.SeparatorSpace))
+          format += " ";
+
+        format += "HH:mm";
+
+        if (_mode.HasFlag(Mode.WithSeconds))
+          format += ":ss";
+
+        if (_mode.HasFlag(Mode.WithMilliseconds3))
+          format += ".fff";
+        else if (_mode.HasFlag(Mode.WithMilliseconds6))
+          format += ".ffffff";
+
+        if (_mode.HasFlag(Mode.WithZ))
+          format += "Z";
+        else if (_mode.HasFlag(Mode.WithOffset))
+          format += "zzz";
+
+        var dateOffset = DateTimeOffset.ParseExact(input, format, CultureInfo.InvariantCulture);
+
+        // workaroud: DateTimeOffset.ParseExact parses date without an offset (2345-10-20 12:34) as local dates with UTC variant off by local offset
+        var date = _mode.HasFlag(Mode.WithOffset) ? dateOffset.UtcDateTime : dateOffset.DateTime;
+
+        var targetKind = _mode.HasFlag(Mode.AsUTC) ? DateTimeKind.Utc : DateTimeKind.Unspecified;
+
+        return DateTime.SpecifyKind(date, targetKind);
+      }
+
+      private static bool IsGenericType(Type type) => type.IsGenericType;
+      private static bool IsNullableType(Type t) => IsGenericType(t) && t.GetGenericTypeDefinition() == typeof(Nullable<>);
     }
   }
 }
