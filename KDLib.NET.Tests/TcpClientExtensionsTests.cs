543a14ad4b117ea87f9c77d3284a21fcdbbee345
Krystian Dużyński <krystian.duzynski@gmail.com>
2020-07-25 16:39:06 +0200

    Added TcpClientConnectAsyncExtensions class

---
+++ KDLib.NET.Tests/TcpClientExtensionsTests.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Net.Sockets;
+using System.Threading;
+using Xunit;
+
+namespace KDLib.NET.Tests
+{
+  public class TcpClientExtensionsTests
+  {
+    [Fact]
+    public async void Connect()
+    {
+      var client = new TcpClient();
+
+      Assert.Null(await Record.ExceptionAsync(() => client.ConnectAsync("8.8.8.8", 53)));
+    }
+
+    [Fact]
+    public async void ConnectTimeout()
+    {
+      var client = new TcpClient();
+
+      await Assert.ThrowsAsync<TimeoutException>(() => client.ConnectAsync("8.8.8.8", 53, TimeSpan.FromMilliseconds(2)));
+    }
+
+    [Fact]
+    public async void ConnectCancelled()
+    {
+      var client1 = new TcpClient();
+      var client2 = new TcpClient();
+
+      var c = new CancellationTokenSource(TimeSpan.FromMilliseconds(100));
+      await Assert.ThrowsAsync<OperationCanceledException>(() => client1.ConnectAsync("8.8.8.7", 53, c.Token));
+
+      await Assert.ThrowsAsync<OperationCanceledException>(() => client2.ConnectAsync("8.8.8.7", 53, TimeSpan.FromMilliseconds(2000), c.Token));
+    }
+  }
+}
---
+++ KDLib.NET/TcpClientExtensions.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KDLib
+{
+  public static class TcpClientConnectAsyncExtensions
+  {
+    public static Task ConnectAsync(this TcpClient client, IPAddress address, int port, TimeSpan timeout)
+      => client.ConnectAsync(address, port, timeout, CancellationToken.None);
+
+    public static Task ConnectAsync(this TcpClient client, IPAddress address, int port, CancellationToken cancellationToken)
+      => client.ConnectAsync(address, port, Timeout.InfiniteTimeSpan, cancellationToken);
+
+    public static async Task ConnectAsync(this TcpClient client, IPAddress address, int port, TimeSpan timeout, CancellationToken cancellationToken)
+    {
+      var connectTask = client.ConnectAsync(address, port);
+
+      var cancellationTaskSource = new TaskCompletionSource<bool>();
+
+      using (var timeoutCancellationTokenSource = new CancellationTokenSource()) {
+        using (cancellationToken.Register(() => cancellationTaskSource.SetResult(true))) {
+          var timeoutTask = Task.Delay(timeout, timeoutCancellationTokenSource.Token);
+          var cancellationTask = cancellationTaskSource.Task;
+
+          var completed = await Task.WhenAny(connectTask, timeoutTask, cancellationTask);
+
+          timeoutCancellationTokenSource.Cancel();
+
+          if (completed == connectTask) { }
+          else if (completed == timeoutTask) {
+            client.Close();
+            throw new TimeoutException();
+          }
+          else if (completed == cancellationTask) {
+            client.Close();
+            cancellationToken.ThrowIfCancellationRequested();
+          }
+        }
+      }
+    }
+
+    public static Task ConnectAsync(this TcpClient client, string host, int port, TimeSpan timeout)
+      => client.ConnectAsync(IPAddress.Parse(host), port, timeout);
+
+    public static Task ConnectAsync(this TcpClient client, string host, int port, CancellationToken cancellationToken)
+      => client.ConnectAsync(IPAddress.Parse(host), port, cancellationToken);
+
+    public static Task ConnectAsync(this TcpClient client, string host, int port, TimeSpan timeout, CancellationToken cancellationToken)
+      => client.ConnectAsync(IPAddress.Parse(host), port, timeout, cancellationToken);
+  }
+}
