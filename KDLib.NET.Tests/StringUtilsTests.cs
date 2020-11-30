9b235614cd57b6beca1afe19df57d9ae73d7a4d0
Krystian Dużyński <krystian.duzynski@gmail.com>
2020-12-01 00:28:06 +0100

    StringUtils: use shared Random class instance in GenerateRandomString

---
+++ KDLib.NET.Tests/StringUtilsTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace KDLib.NET.Tests
+{
+  public class StringUtilsTests
+  {
+    [Fact]
+    public void RandomTest()
+    {
+      var t1 = StringUtils.GenerateRandomString(5);
+      var t2 = StringUtils.GenerateRandomString(5);
+
+      Assert.Equal(5, t1.Length);
+      Assert.NotEqual(t1, t2);
+    }
+  }
+}
--- KDLib.NET/StringUtils.cs
+++ KDLib.NET/StringUtils.cs
@@ -6,15 +6,18 @@ namespace KDLib
 {
   public static class StringUtils
   {
+    private static readonly Random _stringRandom = new Random();
+
     public static string GenerateRandomString(
         int length,
         string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
     {
       var stringChars = new char[length];
-      var random = new Random();
 
-      for (var i = 0; i < stringChars.Length; i++)
-        stringChars[i] = chars[random.Next(chars.Length)];
+      lock (_stringRandom) {
+        for (var i = 0; i < stringChars.Length; i++)
+          stringChars[i] = chars[_stringRandom.Next(chars.Length)];
+      }
 
       return new string(stringChars);
     }
