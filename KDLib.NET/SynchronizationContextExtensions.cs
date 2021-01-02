2200a91b4b498c7ec68ff428436562683427e2b6
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-01-02 18:30:55 +0100

    SynchronizationContextExtensions: removed unneeded overloads

--- KDLib.NET/SynchronizationContextExtensions.cs
+++ KDLib.NET/SynchronizationContextExtensions.cs
@@ -6,8 +6,6 @@ namespace KDLib
 {
   public static class SynchronizationContextExtensions
   {
-    public static void Post(this SynchronizationContext ctx, SendOrPostCallback d) => ctx.Post(d, null);
     public static void Post(this SynchronizationContext ctx, Action d) => ctx.Post(_ => { d(); }, null);
-    public static void Post(this SynchronizationContext ctx, Func<Task> d) => ctx.Post(_ => { d(); }, null);
   }
 }
