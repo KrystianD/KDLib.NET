6b2b6f44ba3570ed50c295edc86a54305a2c81c5
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-01-05 17:08:49 +0100

    added EnumTraits

---
+++ KDLib.NET.Tests/EnumTraitsTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Runtime.Serialization;
+using Xunit;
+
+namespace KDLib.NET.Tests
+{
+  public class EnumTraitsTests
+  {
+    private enum TestEnum
+    {
+      [EnumMember(Value = "Value 1")]
+      Value1 = -1,
+
+      [EnumMember(Value = "Value 2")]
+      Value2 = 2,
+
+      [EnumMember(Value = "Value 3")]
+      Value3,
+    }
+
+    [Fact]
+    public void TestTraits()
+    {
+      Assert.Collection(EnumTraits<TestEnum>.EnumValues.OrderBy(x => (long)x),
+                        x => Assert.Equal(TestEnum.Value1, x),
+                        x => Assert.Equal(TestEnum.Value2, x),
+                        x => Assert.Equal(TestEnum.Value3, x));
+
+      Assert.False(EnumTraits<TestEnum>.IsEmpty);
+      Assert.Equal(-1, EnumTraits<TestEnum>.MinValue);
+      Assert.Equal(3, EnumTraits<TestEnum>.MaxValue);
+    }
+
+    [Fact]
+    public void TestFindByMemberValue()
+    {
+      Assert.Equal(TestEnum.Value1, EnumTraits<TestEnum>.FindByMemberValue("Value 1"));
+      Assert.Equal(TestEnum.Value2, EnumTraits<TestEnum>.FindByMemberValue("Value 2"));
+      Assert.Null(EnumTraits<TestEnum>.FindByMemberValue("other"));
+    }
+  }
+}
---
+++ KDLib.NET/EnumTraits.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace KDLib
+{
+  [PublicAPI]
+  public static class EnumTraits<TEnum> where TEnum : struct, Enum
+  {
+    private static HashSet<TEnum> _valuesSet;
+    private static Dictionary<string, TEnum> _memberValueToEnum;
+
+    static EnumTraits()
+    {
+      var type = typeof(TEnum);
+      var underlyingType = Enum.GetUnderlyingType(type);
+
+      EnumValues = (TEnum[])Enum.GetValues(typeof(TEnum));
+      _valuesSet = new HashSet<TEnum>(EnumValues);
+      _memberValueToEnum = EnumValues
+                           .Select(x => (x.GetMemberValue(), x))
+                           .Distinct(x => x.Item1)
+                           .ToDictionary(x => x.Item1, x => x.Item2);
+
+      var longValues =
+          EnumValues
+              .Select(v => Convert.ChangeType(v, underlyingType))
+              .Select(Convert.ToInt64)
+              .ToList();
+
+      IsEmpty = longValues.Count == 0;
+      if (!IsEmpty) {
+        var sorted = longValues.OrderBy(v => v).ToList();
+        MinValue = sorted.Min();
+        MaxValue = sorted.Max();
+      }
+    }
+
+    public static bool IsEmpty { get; }
+    public static long MinValue { get; }
+    public static long MaxValue { get; }
+    public static TEnum[] EnumValues { get; }
+
+    public static bool IsValid(TEnum value) => _valuesSet.Contains(value);
+
+    public static TEnum? FindByMemberValue(string value)
+    {
+      if (_memberValueToEnum.TryGetValue(value, out var val))
+        return val;
+      return null;
+    }
+  }
+}
