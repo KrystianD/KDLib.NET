e9a13f4c993b2ca8468611476bba8a9a965d6cf5
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-03-14 18:06:49 +0100

    BinaryEncoder: added Hex encoding support

--- KDLib.NET/BinaryEncoder.cs
+++ KDLib.NET/BinaryEncoder.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 
 namespace KDLib
@@ -7,6 +9,7 @@ namespace KDLib
   {
     Base64,
     Base62,
+    Hex,
   }
 
   [PublicAPI]
@@ -17,15 +20,25 @@ namespace KDLib
       switch (encoding) {
         case BinaryEncoding.Base64: return Convert.ToBase64String(data);
         case BinaryEncoding.Base62: return Base62Converter.Encode(data);
+        case BinaryEncoding.Hex:
+          return data.Select(x => x.ToString("x2"))
+                     .JoinString();
         default: throw new ArgumentOutOfRangeException(nameof(encoding), encoding, null);
       }
     }
+
+    public static string Encode(IEnumerable<byte> data, BinaryEncoding encoding) => Encode(data.ToArray(), encoding);
 
     public static byte[] Decode(string data, BinaryEncoding encoding)
     {
       switch (encoding) {
         case BinaryEncoding.Base64: return Convert.FromBase64String(data);
         case BinaryEncoding.Base62: return Base62Converter.Decode(data);
+        case BinaryEncoding.Hex:
+          return Enumerable.Range(0, data.Length)
+                           .Where(x => x % 2 == 0)
+                           .Select(x => Convert.ToByte(data.Substring(x, 2), 16))
+                           .ToArray();
         default: throw new ArgumentOutOfRangeException(nameof(encoding), encoding, null);
       }
     }
