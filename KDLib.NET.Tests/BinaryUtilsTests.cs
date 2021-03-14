c4fb442c7a5c3cfd53adc47d0c2a2787c5d712a7
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-03-14 18:34:41 +0100

    BinaryUtils: added SwapBytes and SwapStructureEndianness

---
+++ KDLib.NET.Tests/BinaryUtilsTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace KDLib.NET.Tests
+{
+  // ReSharper disable RedundantCast
+  public class BinaryUtilsTests
+  {
+    void AssertSwap(short v) => Assert.Equal(BitConverter.ToInt16(BitConverter.GetBytes(v).Reverse().ToArray()), BinaryUtils.SwapBytes(v));
+    void AssertSwap(ushort v) => Assert.Equal(BitConverter.ToUInt16(BitConverter.GetBytes(v).Reverse().ToArray()), BinaryUtils.SwapBytes(v));
+    void AssertSwap(int v) => Assert.Equal(BitConverter.ToInt32(BitConverter.GetBytes(v).Reverse().ToArray()), BinaryUtils.SwapBytes(v));
+    void AssertSwap(uint v) => Assert.Equal(BitConverter.ToUInt32(BitConverter.GetBytes(v).Reverse().ToArray()), BinaryUtils.SwapBytes(v));
+    void AssertSwap(long v) => Assert.Equal(BitConverter.ToInt64(BitConverter.GetBytes(v).Reverse().ToArray()), BinaryUtils.SwapBytes(v));
+    void AssertSwap(ulong v) => Assert.Equal(BitConverter.ToUInt64(BitConverter.GetBytes(v).Reverse().ToArray()), BinaryUtils.SwapBytes(v));
+    void AssertSwap(float v) => Assert.Equal(BitConverter.ToSingle(BitConverter.GetBytes(v).Reverse().ToArray()), BinaryUtils.SwapBytes(v));
+    void AssertSwap(double v) => Assert.Equal(BitConverter.ToDouble(BitConverter.GetBytes(v).Reverse().ToArray()), BinaryUtils.SwapBytes(v));
+
+    [Fact]
+    public void SwapShortTest()
+    {
+      AssertSwap((short)1234);
+      AssertSwap((short)-1234);
+
+      AssertSwap((ushort)1234);
+
+      AssertSwap((int)1234);
+      AssertSwap((int)-1234);
+
+      AssertSwap((uint)1234);
+
+      AssertSwap((long)1234);
+      AssertSwap((long)-1234);
+
+      AssertSwap((ulong)1234);
+
+      AssertSwap((float)1234.0f);
+      AssertSwap((float)-1234.0f);
+
+      AssertSwap((double)1234.0);
+      AssertSwap((double)-1234.0);
+    }
+
+    private struct TestStruct
+    {
+      public short ValShort;
+      public ushort ValUshort;
+      public int ValInt;
+      public uint ValUint;
+      public long ValLong;
+      public ulong ValUlong;
+      public float ValFloat;
+      public double ValDouble;
+    }
+
+    [Fact]
+    public void StructSwapTest()
+    {
+      var v = new TestStruct() {
+          ValShort = -1234,
+          ValUshort = 1234,
+          ValInt = -1234,
+          ValUint = 1234,
+          ValLong = -1234,
+          ValUlong = 1234,
+          ValFloat = 1234.0f,
+          ValDouble = 1234.0,
+      };
+
+      BinaryUtils.SwapStructureEndianness(ref v);
+
+      Assert.Equal(BinaryUtils.SwapBytes((short)-1234), v.ValShort);
+      Assert.Equal(BinaryUtils.SwapBytes((ushort)1234), v.ValUshort);
+      Assert.Equal(BinaryUtils.SwapBytes((int)-1234), v.ValInt);
+      Assert.Equal(BinaryUtils.SwapBytes((uint)1234), v.ValUint);
+      Assert.Equal(BinaryUtils.SwapBytes((long)-1234), v.ValLong);
+      Assert.Equal(BinaryUtils.SwapBytes((ulong)1234), v.ValUlong);
+      Assert.Equal(BinaryUtils.SwapBytes((float)1234.0f), v.ValFloat);
+      Assert.Equal(BinaryUtils.SwapBytes((double)1234.0), v.ValDouble);
+    }
+  }
+}
--- KDLib.NET/BinaryUtils.cs
+++ KDLib.NET/BinaryUtils.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using JetBrains.Annotations;
 
@@ -27,6 +29,82 @@ namespace KDLib
       handle.Free();
 
       return bytes;
+    }
+
+    public static short SwapBytes(short x) => (short)SwapBytes((ushort)x);
+
+    public static ushort SwapBytes(ushort x)
+    {
+      return (ushort)(((x & 0xff00) >> 8) |
+                      ((x & 0x00ff) << 8));
+    }
+
+    public static int SwapBytes(int x) => (int)SwapBytes((uint)x);
+
+    public static uint SwapBytes(uint x)
+    {
+      return ((x & 0xff000000) >> 24) |
+             ((x & 0x00ff0000) >> 8) |
+             ((x & 0x0000ff00) << 8) |
+             ((x & 0x000000ff) << 24);
+    }
+
+    public static long SwapBytes(long x) => (long)SwapBytes((ulong)x);
+
+    public static ulong SwapBytes(ulong x)
+    {
+      x = ((x & 0xffffffff00000000) >> 32) |
+          ((x & 0x00000000ffffffff) << 32);
+      x = ((x & 0xffff0000ffff0000) >> 16) |
+          ((x & 0x0000ffff0000ffff) << 16);
+      x = ((x & 0xff00ff00ff00ff00) >> 8) |
+          ((x & 0x00ff00ff00ff00ff) << 8);
+      return x;
+    }
+
+#if NETSTANDARD2_0
+    public static float SwapBytes(float x)
+    {
+      var bytes = BitConverter.GetBytes(x);
+      Array.Reverse(bytes);
+      return BitConverter.ToSingle(bytes, 0);
+    }
+#else
+    public static float SwapBytes(float x) => BitConverter.Int32BitsToSingle(SwapBytes(BitConverter.SingleToInt32Bits(x)));
+#endif
+    public static double SwapBytes(double x) => BitConverter.Int64BitsToDouble(SwapBytes(BitConverter.DoubleToInt64Bits(x)));
+
+    [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+    public static void SwapStructureEndianness<T>(ref T obj)
+    {
+      foreach (var fi in typeof(T).GetFields()) {
+        switch (fi.GetValue(obj)) {
+          case short v:
+            fi.SetValueDirect(__makeref(obj), SwapBytes(v));
+            break;
+          case ushort v:
+            fi.SetValueDirect(__makeref(obj), SwapBytes(v));
+            break;
+          case int v:
+            fi.SetValueDirect(__makeref(obj), SwapBytes(v));
+            break;
+          case uint v:
+            fi.SetValueDirect(__makeref(obj), SwapBytes(v));
+            break;
+          case long v:
+            fi.SetValueDirect(__makeref(obj), SwapBytes(v));
+            break;
+          case ulong v:
+            fi.SetValueDirect(__makeref(obj), SwapBytes(v));
+            break;
+          case float v:
+            fi.SetValueDirect(__makeref(obj), SwapBytes(v));
+            break;
+          case double v:
+            fi.SetValueDirect(__makeref(obj), SwapBytes(v));
+            break;
+        }
+      }
     }
   }
 }
