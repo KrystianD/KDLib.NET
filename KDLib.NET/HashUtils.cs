5d1a9a3fa79971d7c948b385526894c781ec6d9f
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-06-23 16:03:26 +0200

    HashUtils: added more hash types, unified methods, separate methods for binary/hex hash and for byte/string input

--- KDLib.NET/HashUtils.cs
+++ KDLib.NET/HashUtils.cs
@@ -1,26 +1,41 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Security.Cryptography;
 using System.Text;
 using JetBrains.Annotations;
 
+// ReSharper disable InconsistentNaming
+
 namespace KDLib
 {
+  public enum hashType
+  {
+    MD5,
+    SHA1,
+    SHA256,
+    SHA384,
+    SHA512,
+  }
+
   [PublicAPI]
   public static class HashUtils
   {
-    public static string CalculateMD5Hash(string input)
+    // MD5
+    public static byte[] CalculateHash(hashType hashType, byte[] input)
     {
-      using (MD5 hash = MD5.Create()) {
-        byte[] data = hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-        return StringUtils.BinaryToHex(data);
-      }
+      using HashAlgorithm hash = hashType switch {
+          hashType.MD5 => MD5.Create(),
+          hashType.SHA1 => SHA1.Create(),
+          hashType.SHA256 => SHA256.Create(),
+          hashType.SHA384 => SHA384.Create(),
+          hashType.SHA512 => SHA512.Create(),
+          _ => throw new ArgumentException("Invalid hash type"),
+      };
+      return hash!.ComputeHash(input);
     }
 
-    public static string CalculateSHA256Hash(string input)
-    {
-      using (SHA256 hash = SHA256.Create()) {
-        byte[] data = hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-        return StringUtils.BinaryToHex(data);
-      }
-    }
+    public static byte[] CalculateHash(hashType hashType, string input) => CalculateHash(hashType, Encoding.UTF8.GetBytes(input));
+
+    public static string CalculateHashHex(hashType hashType, byte[] input) => StringUtils.BinaryToHex(CalculateHash(hashType, input));
+    public static string CalculateHashHex(hashType hashType, string input) => CalculateHashHex(hashType, Encoding.UTF8.GetBytes(input));
   }
 }
