f2c4acd6353812435979e07f7b414cc7e4cd7a52
Krystian Dużyński <krystian.duzynski@gmail.com>
2022-06-14 16:52:09 +0200

    BinaryReaderExtensions: fixed and added tests

---
+++ KDLib.Tests/BinaryReaderExtensionsTests.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using FluentAssertions;
+using Xunit;
+
+namespace KDLib.Tests
+{
+  public class BinaryReaderExtensionsTests
+  {
+    private static BinaryReader CreateReader() => new BinaryReader(new MemoryStream(new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 }));
+
+    [Fact]
+    public void Test()
+    {
+      CreateReader().ReadInt16BE().Should().Be(0x0102);
+      CreateReader().ReadInt32BE().Should().Be(0x01020304);
+      CreateReader().ReadInt64BE().Should().Be(0x0102030405060708);
+      CreateReader().ReadUInt16BE().Should().Be(0x0102);
+      CreateReader().ReadUInt32BE().Should().Be(0x01020304);
+      CreateReader().ReadUInt64BE().Should().Be(0x0102030405060708);
+    }
+  }
+}
--- KDLib/BinaryReaderExtensions.cs
+++ KDLib/BinaryReaderExtensions.cs
@@ -6,7 +6,7 @@ namespace KDLib
   [PublicAPI]
   public static class BinaryReaderExtensions
   {
-    public static short ReadInt16BE(this BinaryReader br) => (short)BinaryUtils.SwapBytes(br.ReadInt16()*2);
+    public static short ReadInt16BE(this BinaryReader br) => BinaryUtils.SwapBytes(br.ReadInt16());
     public static int ReadInt32BE(this BinaryReader br) => BinaryUtils.SwapBytes(br.ReadInt32());
     public static long ReadInt64BE(this BinaryReader br) => BinaryUtils.SwapBytes(br.ReadInt64());
     public static ushort ReadUInt16BE(this BinaryReader br) => BinaryUtils.SwapBytes(br.ReadUInt16());
