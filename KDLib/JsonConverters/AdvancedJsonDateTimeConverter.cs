3609ab67c25a41172b8233cd95e15652efd2621e
Krystian Dużyński <krystian.duzynski@gmail.com>
2022-06-15 14:05:04 +0200

    JsonConverters/AdvancedJsonDateTimeConverter: return NotSupportedException instead of NotImplementedException in WriteJson

--- KDLib/JsonConverters/AdvancedJsonDateTimeConverter.cs
+++ KDLib/JsonConverters/AdvancedJsonDateTimeConverter.cs
@@ -51,7 +51,7 @@ namespace KDLib.JsonConverters
 
     public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
     {
-      throw new NotImplementedException();
+      throw new NotSupportedException();
     }
 
     public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
