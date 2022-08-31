5f6802294c6797535789c132af78074921516544
Krystian Dużyński <krystian.duzynski@gmail.com>
2022-08-31 22:58:37 +0200

    ReflectionExtensions: added CreateNullableTypeFrom

--- KDLib/ReflectionUtils.cs
+++ KDLib/ReflectionUtils.cs
@@ -10,7 +10,13 @@ namespace KDLib
   {
     public static IList CreateListInstance(Type itemType)
     {
-      return (IList) Activator.CreateInstance(typeof(List<>).MakeGenericType(itemType));
+      return (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(itemType));
+    }
+
+    public static Type CreateNullableTypeFrom(Type type)
+    {
+      type = Nullable.GetUnderlyingType(type) ?? type;
+      return type.IsValueType ? typeof(Nullable<>).MakeGenericType(type) : type;
     }
   }
 }
