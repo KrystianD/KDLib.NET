307ee0baaf04e57999deb48ccbb2b1e9356d3d47
Krystian Dużyński <krystian.duzynski@gmail.com>
2023-10-16 18:51:37 +0200

    GlobalExtensions: added LetAsync

--- KDLib/GlobalExtensions.cs
+++ KDLib/GlobalExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
 
 namespace KDLib
@@ -7,5 +8,11 @@ namespace KDLib
   public static class GlobalExtensions
   {
     public static R Let<T, R>(this T value, Func<T, R> f) => f(value);
+
+    public static async Task<R> LetAsync<T, R>(this Task<T> task, Func<T, R> f)
+    {
+      var value = await task;
+      return f(value);
+    }
   }
 }
