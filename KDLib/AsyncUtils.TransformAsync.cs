dece956ce3f815d9c24df85a6241722f9446f798
Krystian Dużyński <krystian.duzynski@gmail.com>
2024-06-06 17:13:26 +0200

    AsyncUtils/TransformAsync: fixed running TransformAsync tasks on passed taskScheduler, added cancellation token support

--- KDLib/AsyncUtils.TransformAsync.cs
+++ KDLib/AsyncUtils.TransformAsync.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,51 +9,70 @@ namespace KDLib
 {
   public static partial class AsyncUtils
   {
+    [SuppressMessage("ReSharper", "AccessToDisposedClosure")]
     public static async Task<List<TOutput>> TransformAsync<TInput, TOutput>(IEnumerable<TInput> input, int maxRunningTasks,
                                                                             Func<TInput, Task<TOutput>> processor,
-                                                                            TaskScheduler taskScheduler = null)
+                                                                            TaskScheduler? taskScheduler = null,
+                                                                            CancellationToken token = default)
     {
-      if (taskScheduler == null)
-        taskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+      using var semaphore = new SemaphoreSlim(maxRunningTasks);
 
-      return await Task.Factory.StartNew(
-          async () => {
-            using var semaphore = new SemaphoreSlim(maxRunningTasks);
+      async Task<TOutput> ItemProcessorOnCurrentContext(TInput item)
+      {
+        await semaphore.WaitAsync(token);
+        try {
+          return await processor(item);
+        }
+        finally {
+          semaphore.Release();
+        }
+      }
 
-            var tasks = input.Select(async item => {
-              // ReSharper disable once AccessToDisposedClosure
-              await semaphore.WaitAsync();
-              try {
-                return await processor(item);
-              }
-              finally {
-                // ReSharper disable once AccessToDisposedClosure
-                semaphore.Release();
-              }
-            });
+      async Task<TOutput> ItemProcessorOnTaskScheduler(TInput item)
+      {
+        await semaphore.WaitAsync(token);
+        try {
+          var task = await Task.Factory.StartNew(async () => await processor(item),
+                                                 token,
+                                                 TaskCreationOptions.None,
+                                                 taskScheduler);
+          return await task;
+        }
+        finally {
+          semaphore.Release();
+        }
+      }
 
-            return (await Task.WhenAll(tasks)).ToList();
-          },
-          CancellationToken.None,
-          TaskCreationOptions.None,
-          taskScheduler).Unwrap();
+      var tasks = taskScheduler == null
+          ? input.Select(ItemProcessorOnCurrentContext)
+          : input.Select(ItemProcessorOnTaskScheduler);
+
+      var res = await Task.WhenAll(tasks);
+
+      return res.ToList();
     }
 
     public static Task<List<KeyValuePair<TInput, TOutput>>> TransformMapAsync<TInput, TOutput>(IEnumerable<TInput> input, int maxRunningTasks,
                                                                                                Func<TInput, Task<TOutput>> processor,
-                                                                                               TaskScheduler taskScheduler = null)
-    {
-      return TransformAsync(input, maxRunningTasks,
-                            async val => new KeyValuePair<TInput, TOutput>(val, await processor(val)),
-                            taskScheduler);
-    }
+                                                                                               TaskScheduler? taskScheduler = null,
+                                                                                               CancellationToken token = default)
+      => TransformAsync(input: input,
+                        maxRunningTasks: maxRunningTasks,
+                        processor: async val => new KeyValuePair<TInput, TOutput>(val, await processor(val)),
+                        taskScheduler: taskScheduler,
+                        token: token);
 
     public static async Task<List<TOutput>> TransformInChunksAsync<TInput, TOutput>(IEnumerable<TInput> input, int itemsPerTask, int maxRunningTasks,
                                                                                     Func<IReadOnlyList<TInput>, Task<List<TOutput>>> processor,
-                                                                                    TaskScheduler taskScheduler = null)
+                                                                                    TaskScheduler? taskScheduler = null,
+                                                                                    CancellationToken token = default)
     {
       var chunks = input.Chunk(itemsPerTask);
-      var res = await TransformAsync(chunks, maxRunningTasks, processor, taskScheduler: taskScheduler);
+      var res = await TransformAsync(input: chunks,
+                                     maxRunningTasks: maxRunningTasks,
+                                     processor: processor,
+                                     taskScheduler: taskScheduler,
+                                     token: token);
       return res.SelectMany(x => x).ToList();
     }
   }
